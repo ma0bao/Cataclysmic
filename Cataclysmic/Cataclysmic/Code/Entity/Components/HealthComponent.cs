9921ce475c02f9722836f4860ce06ccc7fb133c0
Thomas Liew <257094@friscoisd.org>
2026-03-02 12:09:08 -0600

    Created healthComponent, ability to damge, and way to draw healthbar in drawex.

---
+++ Cataclysmic/Cataclysmic/Code/Entity/Components/HealthComponent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cataclysmic
+{
+    public class HealthComponent
+    {
+        public int maxHealth;
+        public int currentHealth;
+        public bool isAlive;
+
+        // For when health is the same as maxhealth
+        public HealthComponent(int _maxHealth)
+        {
+            maxHealth = _maxHealth;
+            currentHealth = _maxHealth;
+            isAlive = true;
+        }
+
+        //for if you want health to be different from maxhealth
+        public HealthComponent(int _maxHealth, int _currentHealth)
+        {
+            maxHealth = _maxHealth;
+            currentHealth = _currentHealth;
+            isAlive = true;
+        }
+
+        //Entity has a damge thing. do you want to do it here?
+        public bool Damage(Entity cause, int _damage)
+        {
+            if (currentHealth - _damage > 0)
+            {
+                currentHealth -= _damage;
+                return true;
+            }
+
+            if (currentHealth - _damage <= 0)
+            {
+                isAlive = false;
+
+            }
+            return false;
+        }
+
+
+    }
+}
