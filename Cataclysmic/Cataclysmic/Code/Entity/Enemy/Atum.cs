592b55985f2f3df61814fe526c9a8809525d5231
Zackariya Aggour <214826@friscoisd.org>
2026-03-23 12:14:47 -0500

    Ra

--- Cataclysmic/Cataclysmic/Code/Entity/Enemy/Atum.cs
+++ Cataclysmic/Cataclysmic/Code/Entity/Enemy/Atum.cs
@@ -24,21 +24,38 @@ namespace Cataclysmic
 
         AttackStates currentState = AttackStates.Center;
 
-        public void SetStateToCenter()
-        {
-            SetNewTargetPosition(Game1.BOUNDS.Center);
-            currentState = AttackStates.Center;
-        }
+        EventTimer cooldownTimer;
 
+        AttackStates next;
         public Atum(Rectangle destRect, Player player) : base(Game1.texture_player, destRect)
         {
             target = player;
         }
 
+        private void SetStateToCenter()
+        {
+            SetNewTargetPosition(Game1.BOUNDS.Center);
+            currentState = AttackStates.Center;
+        }
+
+        private void SetStateToCooldown(float time, AttackStates nextState)
+        {
+            cooldownTimer = new EventTimer(time);
+            next = nextState;
+            currentState = AttackStates.Cooldown;
+        }
+
         public override void Update(GameTime gameTime)
         {
             #region Get Target Based On State
-            
+
+            #endregion
+
+            #region Update Based On State
+            if (currentState == AttackStates.Center)
+            {
+                base.Update(gameTime);
+            }
             #endregion
         }
 
