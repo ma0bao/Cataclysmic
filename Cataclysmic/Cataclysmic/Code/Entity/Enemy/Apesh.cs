9acf854258582fc4b941fb546fec1826c02f42b8
Zackariya Aggour <214826@friscoisd.org>
2026-03-23 11:15:56 -0500

    Tutrtle, no damage or collision yet

--- Cataclysmic/Cataclysmic/Code/Entity/Enemy/Apesh.cs
+++ Cataclysmic/Cataclysmic/Code/Entity/Enemy/Apesh.cs
@@ -13,7 +13,8 @@ namespace Cataclysmic
             Track,
             Cooldown,
             Slam,
-            Spin
+            Spin,
+            Run
         }
 
         AttackState currentState = AttackState.Track;
@@ -21,7 +22,9 @@ namespace Cataclysmic
         Player player;
 
         EventTimer spinTimer;
-        EventTimer agroCooldown;
+
+        EventTimer timeToSpin;
+
         EventTimer cooldownTimer;
 
         public Apesh(Rectangle destRect, Player p) : base(Game1.texture_player, destRect)
@@ -30,8 +33,11 @@ namespace Cataclysmic
             SetNewTargetPosition(player.renderData.Position);
             distanceToBeAtTarget = 100;
             spinTimer = new EventTimer(10);
-            agroCooldown = new EventTimer(5);
             cooldownTimer = new EventTimer(.4f);
+            timeToSpin = new EventTimer(1.5f);
+            turnSpeed = 500;
+            moveData.maxSpeed = 200;
+            spinTimer.Unpause();
         }
 
         public override void Update(GameTime gameTime)
@@ -52,6 +58,12 @@ namespace Cataclysmic
                     cooldownTimer.Restart();
                     currentState = AttackState.Cooldown;
                 }
+                if (spinTimer.Done)
+                {
+                    spinTimer.Restart();
+                    currentState = AttackState.Spin;
+                }
+                spinTimer.Update();
             }
             else if (currentState == AttackState.Cooldown)
             {
@@ -62,19 +74,51 @@ namespace Cataclysmic
             else if (currentState == AttackState.Slam)
             {
                 Slam();
-                currentState = AttackState.Track;
+                SetNewTargetPosition(renderData.GetRandomPoint());
+                currentState = AttackState.Run;
             }
             else if (currentState == AttackState.Spin)
             {
+                turnSpeed = 1600f;
+                moveData.maxSpeed = 700f;
+                slowRadius = 0;
                 renderData.rotation += 2; //MAKE SPIN TRIGGERED 
+                base.Update(gameTime);
+                turnSpeed = 500;
+                moveData.maxSpeed = 200;
+                slowRadius = 150;
+
+                if (timeToSpin.Done)
+                {
+                    SetNewTargetPosition(renderData.GetRandomPoint());
+                    currentState = AttackState.Run;
+                }
+
+                timeToSpin.Update();
+            }
+            else if (currentState == AttackState.Run)
+            {
+                if (IsAtTarget())
+                {
+                    spinTimer.Restart();
+                    currentState = AttackState.Track;
+                }
+                base.Update(gameTime);
             }
             #endregion
 
         }
 
+        public override void Draw(float opacity)
+        {
+            if(currentState != AttackState.Spin)
+            renderData.rotation = renderData.GetRotationToTarget(player.renderData.Position);
+            base.Draw(opacity);
+        }
+
         public void Slam()
         {
-            //MAKE SLAM
+            //Make Slam
         }
 
 
